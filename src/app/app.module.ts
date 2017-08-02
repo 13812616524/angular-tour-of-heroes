80c02cb55059a0009bff51c71d8dcf48d7a36fbe
manyan <2811095858@qq.com>
2017-08-02 15:38:42 +0800

    zmy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
 import { RouterModule }   from '@angular/router';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { DashboardComponent }     from './dashboard.component';
 import { HeroService }         from './hero.service';
-
 import { AppRoutingModule }     from './app-routing.module';
+import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +21,13 @@ import { AppRoutingModule }     from './app-routing.module';
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
